384aa927725ad1612903a27b7c3ad3bd3b5acba8
Alex Soong <alexsoong91@gmail.com>
2023-05-02 11:29:36 -0700

    Update wagmi config to provide default jsonrpc provider for mantle

--- app/src/index.js
+++ app/src/index.js
@@ -4,6 +4,7 @@ import App from "./App";
 import { WagmiConfig, createClient, configureChains, chain } from "wagmi";
 import { alchemyProvider } from 'wagmi/providers/alchemy'
 import { publicProvider } from "wagmi/providers/public";
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
 import {
   getDefaultWallets,
   RainbowKitProvider,
@@ -13,28 +14,46 @@ import {
 import "./index.css";
 import "@rainbow-me/rainbowkit/styles.css";
 
+
 export const mantleTestnet = {
   id: 5001,
   name: 'Mantle Testnet',
   network: 'mantle',
+  // iconUrl: '',
+  // iconBackground: '#fff',
   nativeCurrency: {
     decimals: 18,
     name: 'BitDAO',
     symbol: 'BIT',
   },
-  rpcUrls: {
-    public: { http: ["https://rpc.testnet.mantle.xyz/"] },
-    default: { http: ["https://rpc.testnet.mantle.xyz/"] },
+  // rpcUrls: {
+  //   public: {
+  //     http: ["https://rpc.testnet.mantle.xyz"]
+  //   },
+  //   default: {
+  //     http: ["https://rpc.testnet.mantle.xyz"]
+  //   },
+  // },
+  rpcUrls: [
+    "https://rpc.testnet.mantle.xyz"
+  ],
+  blockExplorers: {
+    default: {
+      http: ["https://explorer.testnet.mantlenetwork.io"]
+    },
   },
+  testnet: true
 }
 
 const { chains, provider, webSocketProvider } = configureChains(
+  [chain.goerli, mantleTestnet],
   [
-    chain.goerli,
-    mantleTestnet
-  ],
-  [
-    alchemyProvider({ apiKey: '7OLNUah9mWjItVi7QiJWrlc6xVVdSGn3' }),
+    alchemyProvider(
+      { apiKey: '7OLNUah9mWjItVi7QiJWrlc6xVVdSGn3' }
+    ),
+    jsonRpcProvider({
+      rpc: chain => ({ http: 'https://rpc.testnet.mantle.xyz' }),
+    }),
     publicProvider()
   ]
 );
