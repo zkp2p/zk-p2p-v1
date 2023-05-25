49a781972cf8526db4b124a06a3f5aa28879259d
Alex Soong <alexsoong91@gmail.com>
2023-05-25 13:40:58 -0700

    Add new deployed ramp contract address

--- app/src/helpers/deployed_addresses.ts
+++ app/src/helpers/deployed_addresses.ts
@@ -5,11 +5,11 @@ type Contracts = {
 };
 
 export const contractAddresses: Contracts = {
-  "goerli_legacy": {
-    "ramp": '0x805a3Ae6495Be653dE460685D5FFDD5A538550f1',
-    "fusdc": '0xb685Bdc019DEE17D073746a88bF6a6cDDED8Ae70',
-  },
   "goerli": {
+    "ramp": '0xE62e588756576ECE4a8dCAEBA94dd2889dc6A04c',
+    "fusdc": '0xf6426A1fdE02c3d6f10b4af107cDd7669574E74C',
+  },
+  "goerli_legacy": {
     "ramp": '0x945D14a5c63769f4cf008a2994810940cc0DFd5C',
     "fusdc": '0xf6426A1fdE02c3d6f10b4af107cDd7669574E74C',
   },
