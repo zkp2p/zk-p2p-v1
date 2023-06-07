1672c1c4579c6dc0a2e68f25bb0037b783522e32
Alex Soong <alexsoong91@gmail.com>
2023-06-07 00:58:03 -0700

    Update deployed contract addresses and ramp ABI

--- app/src/helpers/ramp.abi.ts
+++ app/src/helpers/ramp.abi.ts
@@ -10,10 +10,34 @@ export const abi = [
             internalType: "contract IERC20",
             name: "_usdc",
             type: "address"
+         },
+         {
+            internalType: "uint256",
+            name: "_maxAmount",
+            type: "uint256"
          }
       ],
       stateMutability: "nonpayable",
       type: "constructor"
+   },
+   {
+      anonymous: false,
+      inputs: [
+         {
+            indexed: true,
+            internalType: "address",
+            name: "previousOwner",
+            type: "address"
+         },
+         {
+            indexed: true,
+            internalType: "address",
+            name: "newOwner",
+            type: "address"
+         }
+      ],
+      name: "OwnershipTransferred",
+      type: "event"
    },
    {
       inputs: [
@@ -211,6 +235,40 @@ export const abi = [
    },
    {
       inputs: [
+         
+      ],
+      name: "maxAmount",
+      outputs: [
+         {
+            internalType: "uint256",
+            name: "",
+            type: "uint256"
+         }
+      ],
+      stateMutability: "view",
+      type: "function"
+   },
+   {
+      inputs: [
+         {
+            internalType: "bytes32",
+            name: "",
+            type: "bytes32"
+         }
+      ],
+      name: "nullified",
+      outputs: [
+         {
+            internalType: "bool",
+            name: "",
+            type: "bool"
+         }
+      ],
+      stateMutability: "view",
+      type: "function"
+   },
+   {
+      inputs: [
          {
             internalType: "uint256[2]",
             name: "_a",
@@ -227,14 +285,9 @@ export const abi = [
             type: "uint256[2]"
          },
          {
-            internalType: "uint256[22]",
+            internalType: "uint256[26]",
             name: "_signals",
-            type: "uint256[22]"
-         },
-         {
-            internalType: "uint256",
-            name: "claimId",
-            type: "uint256"
+            type: "uint256[26]"
          }
       ],
       name: "onRamp",
@@ -392,6 +445,21 @@ export const abi = [
    },
    {
       inputs: [
+         
+      ],
+      name: "owner",
+      outputs: [
+         {
+            internalType: "address",
+            name: "",
+            type: "address"
+         }
+      ],
+      stateMutability: "view",
+      type: "function"
+   },
+   {
+      inputs: [
          {
             internalType: "uint256",
             name: "_amount",
@@ -409,6 +477,62 @@ export const abi = [
          }
       ],
       name: "postOrder",
+      outputs: [
+         
+      ],
+      stateMutability: "nonpayable",
+      type: "function"
+   },
+   {
+      inputs: [
+         
+      ],
+      name: "renounceOwnership",
+      outputs: [
+         
+      ],
+      stateMutability: "nonpayable",
+      type: "function"
+   },
+   {
+      inputs: [
+         {
+            internalType: "uint256",
+            name: "_maxAmount",
+            type: "uint256"
+         }
+      ],
+      name: "setMaxAmount",
+      outputs: [
+         
+      ],
+      stateMutability: "nonpayable",
+      type: "function"
+   },
+   {
+      inputs: [
+         {
+            internalType: "uint256[17]",
+            name: "_venmoMailserverKeys",
+            type: "uint256[17]"
+         }
+      ],
+      name: "setVenmoMailserverKeys",
+      outputs: [
+         
+      ],
+      stateMutability: "nonpayable",
+      type: "function"
+   },
+   {
+      inputs: [
+         {
+            internalType: "address",
+            name: "newOwner",
+            type: "address"
+         }
+      ],
+      name: "transferOwnership",
       outputs: [
          
       ],
@@ -467,9 +591,9 @@ export const abi = [
             type: "uint256[2]"
          },
          {
-            internalType: "uint256[22]",
+            internalType: "uint256[26]",
             name: "input",
-            type: "uint256[22]"
+            type: "uint256[26]"
          }
       ],
       name: "verifyProof",
